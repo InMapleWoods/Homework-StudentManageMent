65d785b2cc90b038dfadb4805f5b06fc76a25604
InMapleWoods <llf99723@gmail.com>
2020-05-25 17:25:01 +0800

    5/25 新增了单元测试项目

---
+++ dotNetCore/StudentManageUnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StudentManageUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
